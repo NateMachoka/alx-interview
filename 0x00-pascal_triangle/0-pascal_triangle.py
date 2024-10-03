06a3967ce3969c4700b4ffd64c481c49f300b67c
NateMachoka <natemachoka@gmail.com>
2024-10-03 09:51:06 -0700

    0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+def pascal_triangle(n):
+    """Creats Pascal's triangle upto the `n`th level
+
+        Args:
+            n (number): The number of levels to show
+        Returns:
+            A list of lists of integers representing Pascal's triangle
+            [] otherwise
+    """
+    if n <= 0:
+        return []
+
+    triangle = []
+
+    for i in range(n):
+        # Start a new row with '1'
+        row = [1] * (i + 1)
+
+        # Each triangle row has values that are the sum of the two values above
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+
+        triangle.append(row)
+
+    return triangle
